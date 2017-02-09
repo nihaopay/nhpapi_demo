329f59393bc568b6871ae85e91fe61ebba10be96
NihaoPay <dev.admin@aurfy.com>
2017-02-09 14:23:06 +0800

    Update config.php
    change to v1.2

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ $uri = 'http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'.$server;
 //You can view your bearer token in the TMS by logging in and going to Settings -> Certificate -> View API Token.
 define('TOKEN', '4847fed22494dc22b1b1a478b34e374e0b429608f31adf289704b4ea093e60a8');
 
-define("API_URL", "http://api.test.nihaopay.com/v1.1");//for test environment
+define("API_URL", "https://apitest.nihaopay.com/v1.2");//for test environment
 //define("API_URL", "https://api.nihaopay.com/v1.1");//production environment
 
 define("RETURN_URL", "http://phpdemo.aurfy.com/example/response/securepay_notify.php");//you page return url Notice: Return Url can not be localhost
